7f6285a67356dd66a0e2b858c77ef3ffe1906e8f
Guanqun Ma <guanqunma@163.com>
2025-05-28 18:02:12 -0500

    NOT finished spack version

---
+++ create-env.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+export SPACKENV=mfa-TDA-env
+export YAML=$PWD/env.yaml
+
+# create spack environment
+echo "creating spack environment $SPACKENV"
+spack env deactivate > /dev/null 2>&1
+spack env remove -y $SPACKENV > /dev/null 2>&1
+spack env create $SPACKENV $YAML
+
+# activate environment
+echo "activating spack environment"
+spack env activate $SPACKENV
+
+spack add mpich@4
+spack add hdf5@1.14+hl+mpi
+spack add tbb
+spack add eigen
+spack add highfive
+spack add mfa~examples~tests
+
+
+# Optional: add other dependencies you may need
+# spack add zlib
+# spack add fmt
+# spack add diy
+
+# install
+echo "installing dependencies in environment"
+spack install mfa~examples~tests mfa_thread=tbb   # install separately so that MFA_PATH is set for later packages
+export MFA_PATH=`spack location -i mfa`
+
+
+# Install everything else
+echo "Installing remaining dependencies"
+spack install
+
+
+spack env deactivate
+
---
+++ load-env.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# activate the environment
+export SPACKENV=mfa-TDA-env
+spack env deactivate > /dev/null 2>&1
+spack env activate $SPACKENV
+echo "activated spack environment $SPACKENV"
+
+echo "setting flags for building mfa-TDA"
+export MFA_PATH=$(spack location -i mfa)
+export TBB_PATH=$(spack location -i tbb)
+export HDF5_PATH=$(spack location -i hdf5)
+export EIGEN_PATH=$(spack location -i eigen)
+export HIGHFIVE_PATH=$(spack location -i highfive)
+
+# Optional: extend your library path if you plan to run compiled executables
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    export LD_LIBRARY_PATH=$HDF5_PATH/lib:$TBB_PATH/lib:$LD_LIBRARY_PATH
+fi
+
+# Optional: echo them for debugging
+echo "MFA_PATH        = $MFA_PATH"
+echo "TBB_PATH        = $TBB_PATH"
+echo "HDF5_PATH       = $HDF5_PATH"
+echo "EIGEN_PATH      = $EIGEN_PATH"
+echo "HIGHFIVE_PATH   = $HIGHFIVE_PATH"
+# give openMP 1 core for now to prevent using all cores for threading
+# could set a more reasonable number to distribute cores between mpi + openMP
+# export OMP_NUM_THREADS=1
